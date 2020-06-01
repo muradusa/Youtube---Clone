f21474230c70503b2976755d1e55ef09c53b1980
Murad Cholukov <muradusa@gmail.com>
2020-06-01 15:29:01 -0700

    adding get Videos route to Video.js

--- server/routes/video.js
+++ server/routes/video.js
@@ -36,4 +36,16 @@ router.post("/uploadfiles", (req, res) => {
 
 });
 
+
+router.get("/getVideos", (req, res) => {
+
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+
+});
+
 module.exports = router;
