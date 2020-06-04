06a1d97c35eacd3b3bf1d6ced32b5459b647418c
Murad Cholukov <muradusa@gmail.com>
2020-06-04 16:44:34 -0700

    adding ffmpeg

--- server/routes/video.js
+++ server/routes/video.js
@@ -37,6 +37,40 @@ router.post("/uploadfiles", (req, res) => {
 });
 
 
+router.post("/thumbnail", (req, res) => {
+
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+    })
+
+
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3,
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
+
+});
+
+
 router.get("/getVideos", (req, res) => {
 
     Video.find()
