ac5a58d1823c8e39fb8d3114ba73f685dce672e8
Murad Cholukov <muradusa@gmail.com>
2020-06-10 19:52:31 -0700

    adding Subscribe button

--- client/src/components/views/DetailVideoPage/DetailVideoPage.js
+++ client/src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { List, Avatar, Row, Col } from 'antd';
 import axios from 'axios';
 import SideVideo from './Sections/SideVideo';
-// import Subscriber from './Sections/Subscriber';
+import Subscriber from './Sections/Subscriber';
 // import Comments from './Sections/Comments'
 // import LikeDislikes from './Sections/LikeDislikes';
 
@@ -54,7 +54,10 @@ function DetailVideoPage(props) {
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
 
                         <List.Item
-                            // actions={[<LikeDislikes video videoId={videoId} userId={localStorage.getItem('userId')}  />, <Subscriber userTo={Video.writer._id} userFrom={localStorage.getItem('userId')} />]}
+                            actions={[
+                            // <LikeDislikes video videoId={videoId} userId={localStorage.getItem('userId')}  />, 
+                            <Subscriber userTo={Video.writer._id} userFrom={localStorage.getItem('userId')} />
+                            ]}
                         >
                             <List.Item.Meta
                                 avatar={<Avatar src={Video.writer && Video.writer.image} />}
