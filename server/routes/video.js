42256385b1ab4526e3323cfc2f890850449f0157
Murad Cholukov <muradusa@gmail.com>
2020-06-11 19:17:54 -0700

    finished Subscription Page

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 var ffmpeg = require('fluent-ffmpeg');
 
 const { Video } = require("../models/Video");
-// const { Subscriber } = require("../models/Subscriber");
+const { Subscriber } = require("../models/Subscriber");
 // const { auth } = require("../middleware/auth");
 
 var storage = multer.diskStorage({
@@ -111,30 +111,30 @@ router.get("/getVideos", (req, res) => {
 
 });
 
-// router.post("/getSubscriptionVideos", (req, res) => {
+router.post("/getSubscriptionVideos", (req, res) => {
 
 
-//     //Need to find all of the Users that I am subscribing to From Subscriber Collection 
+    //Need to find all of the Users that I am subscribing to From Subscriber Collection 
     
-//     Subscriber.find({ 'userFrom': req.body.userFrom })
-//     .exec((err, subscribers)=> {
-//         if(err) return res.status(400).send(err);
+    Subscriber.find({ 'userFrom': req.body.userFrom })
+    .exec((err, subscribers)=> {
+        if(err) return res.status(400).send(err);
 
-//         let subscribedUser = [];
+        let subscribedUser = [];
 
-//         subscribers.map((subscriber, i)=> {
-//             subscribedUser.push(subscriber.userTo)
-//         })
+        subscribers.map((subscriber, i)=> {
+            subscribedUser.push(subscriber.userTo)
+        })
 
 
-//         //Need to Fetch all of the Videos that belong to the Users that I found in previous step. 
-//         Video.find({ writer: { $in: subscribedUser }})
-//             .populate('writer')
-//             .exec((err, videos) => {
-//                 if(err) return res.status(400).send(err);
-//                 res.status(200).json({ success: true, videos })
-//             })
-//     })
-// });
+        //Need to Fetch all of the Videos that belong to the Users that I found in previous step. 
+        Video.find({ writer: { $in: subscribedUser }})
+            .populate('writer')
+            .exec((err, videos) => {
+                if(err) return res.status(400).send(err);
+                res.status(200).json({ success: true, videos })
+            })
+    })
+});
 
 module.exports = router;
