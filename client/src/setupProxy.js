52a7beb6e7bc97dc6b9f37f5a1fdee54ff1c9b96
Murad Cholukov <muradusa@gmail.com>
2020-06-14 09:01:26 -0700

    node and npm versions

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'https://fathomless-sands-89545.herokuapp.com',
+            // target: 'http://localhost:5000',
             changeOrigin: true,
         })
     );
